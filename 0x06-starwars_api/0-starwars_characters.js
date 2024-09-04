b481828dc6e78c1f63df0d0b556384f4a79978ae
Danmwihoti <danielmwihoti@gmail.com>
2024-09-04 13:26:44 +0300

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,8 +5,8 @@ const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.log('Usage: ./0-starwars_characters.js <Movie ID>');
-    process.exit(1);
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
 // API endpoint for the specified movie 
@@ -15,28 +15,28 @@ const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 // Make the request to the API
 request(url, (error, response, body) => {
     if (error) {
-        console.log(error);
-        return;
+      console.log(error);
+      return;
     }
     // Parse the response body as JSON
     const filmData = JSON.parse(body);
     
     // Check if film data contains characters
     if (!filmData.characters) {
-        console.log('No characters found for this movie.');
-        return;
+      console.log('No characters found for this movie.');
+      return;
     }
 
     // Iterate through the list of character URLs
     filmData.characters.forEach(characterUrl => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-            if (charError) {
-                console.log(charError);
-                return;
-            }
-            // Parse the character data and print the name
-            const characterData = JSON.parse(charBody);
-            console.log(characterData.name);
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.log(charError);
+          return;
+        }
+        // Parse the character data and print the name
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
         });
     });
 });
