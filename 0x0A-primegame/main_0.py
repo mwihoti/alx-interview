4bd2ac80d318133a9e635d85de4637238c29425e
mwihoti <danielmwihoti@gmail.com>
2024-10-03 14:45:29 +0300

    Prime_game

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-isWinner = __import__('0-prime_game').isWinner
+isWinner = __import__('prime').isWinner
 
 
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
--- 0x0A-primegame/prime.py
+++ 0x0A-primegame/prime.py
@@ -2,21 +2,20 @@
 
 """Prime Game"""
 
-
 def isWinner(x, nums):
     """
     Determines the winner of a set of prime numbers
     """
-    if x < 0 or nums is None:
+    if x <0 or nums is None:
         return None
     if x != len(nums):
         return None
-
+    
     # Initialize scores and array of possible prime numbers
     ben = 0
     maria = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a = [1 for x in range(sorted(nums)[-1] +1)]
 
     a[0], a[1] = 0, 0
 
@@ -34,8 +33,6 @@ def isWinner(x, nums):
     if maria > ben:
         return "Maria"
     return None
-
-
 def prime_multiples(ls, x):
     for i in range(2, len(ls)):
         try:
